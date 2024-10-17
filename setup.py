02685ec250152c4fc1595143155327652a5cd2af
agoosecalledbozo <140794442+agoosecalledbozo@users.noreply.github.com>
2024-10-17 17:37:42 +0800

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
-#setup.py
 from setuptools import setup, find_packages
 
 setup(
     name="chessNormalizer",
-    version="0.1.0",
+    use_scm_version=True,
     description="A chess move normalization library for Python.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     author="Kayden",
-    author_email="none",
-    url="none",
+    author_email="btskt613@gmail.com",
+    url="https://github.com/agoosecalledbozo/Chess-Normalizer",
     packages=find_packages(),
     install_requires=["python-chess>=0.31.4"],
+    setup_requires=["setuptools_scm"],  # Required to use setuptools_scm
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
